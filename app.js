826de24a8a96210bee57d683f9b9ae6a7d88d6ac
Jake Young <young4design@gmail.com>
2022-10-10 12:57:06 -0600

    GameArea structure and order

---
+++ app.js
@@ -0,0 +1,4 @@
+const playerScore = 0;
+const computerScore = 0;
+const playerScore_span = document.getElementById("playerScore");
+const computerScore_span = document.getElementById("computerScore");
