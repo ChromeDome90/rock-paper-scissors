bad67dd8f51ffa0566dc2c86dc0998f932db41df
Jake Young <young4design@gmail.com>
2022-10-11 15:04:03 -0600

    Game score counter and styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Rock Paper Scissors Game
 
-const playerScore = 0;
-const computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 const playerScore_span = document.getElementById("playerScore");
 const computerScore_span = document.getElementById("computerScore");
 const scoreBoard_div = document.querySelector(".scoreBoard");
@@ -13,14 +13,65 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click', function() {
-    console.log("You clicked on rock!");
-})
 
-paper_div.addEventListener('click', function() {
-    console.log("You clicked on paper!");
-})
+main();
 
-scissors_div.addEventListener('click', function() {
-    console.log("You clicked on scissors!");
-})
+// Generate random computer choice
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win() {
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+}
+
+function lose() {
+}
+
+function tie() {
+    console.log("TIE!");
+}
+
+// Player choice 
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) { 
+        case "rockscissors": // Player win conditions
+        case "paperrock":
+        case "scissorspaper":
+            win();
+            break;
+        case "rockpaper": // Player lose conditions
+        case "paperscissors":
+        case "scissorsrock":
+            lose();
+            break;
+        case "rockrock": // Tie conditions
+        case "paperpaper":
+        case "scissorsscissors":
+            tie();
+            break;
+    }
+}
+
+// Game buttons for user to choose/click
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");;
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");;
+    })
+}
